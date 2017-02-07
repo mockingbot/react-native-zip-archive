ae6366a55b7e59ddfb6137c23a9aa286cb4d5ffc
Perry Poon <plrthink@gmail.com>
2017-02-07 17:11:10 +0800

    add android example

--- example/index.ios.js
+++ example/index.ios.js
@@ -8,48 +8,15 @@ import React, { Component } from 'react';
 import {
   AppRegistry,
   StyleSheet,
-  Text,
-  View,
-  WebView
+  View
 } from 'react-native';
-import { MainBundlePath, DocumentDirectoryPath } from 'react-native-fs';
-import { unzip } from 'react-native-zip-archive';
+import App from './App';
 
 export default class example extends Component {
-  constructor () {
-    super();
-    this.state = {
-      uri: 'https://github.com/plrthink/react-native-zip-archive'
-    }
-  }
-
-  componentDidMount () {
-    const sourcePath = `${MainBundlePath}/static.zip`;
-    const targetPath = DocumentDirectoryPath;
-
-    unzip(sourcePath, targetPath)
-      .then((path) => {
-        console.log(`unzip file to ${path}`);
-        this.setState({
-          uri: `${path}/static/index.html`
-        })
-      })
-      .catch((error) => {
-        console.log(error);
-      });
-  }
-
   render() {
-    const { uri } = this.state
-
     return (
       <View style={styles.container}>
-        <WebView
-          source={{uri: uri}}
-          style={styles.webView}
-          startInLoadingState={true}
-          scalesPageToFit={true}
-        />
+        <App />
       </View>
     );
   }
@@ -59,11 +26,7 @@ const styles = StyleSheet.create({
   container: {
     flex: 1,
     alignItems: 'stretch',
-  },
-  webView: {
-    flex: 1,
-    marginTop: 20,
-  },
+  }
 });
 
 AppRegistry.registerComponent('example', () => example);
