f23ccd648519e3f9143db31cde28d731a1042491
Richard <wrongch@gmail.com>
2017-08-16 08:47:04 +0800

    Remove createJSModules @overide marker - RN 0.47 compatibility
    [createJSModules is now not required on Android](https://github.com/facebook/react-native/commit/ce6fb337a146e6f261f2afb564aa19363774a7a8) from RN 0.47.
    I actually can't build an app on RN 0.47 without removing this method/@overide marker.
    There is similar PR ([#649](https://github.com/react-community/react-native-image-picker/pull/649))
    and ([#653](https://github.com/react-community/react-native-image-picker/pull/653))

--- android/src/main/java/com/rnziparchive/RNZipArchivePackage.java
+++ android/src/main/java/com/rnziparchive/RNZipArchivePackage.java
@@ -17,7 +17,7 @@ public class RNZipArchivePackage implements ReactPackage {
     return modules;
   }
 
-  @Override
+  // Deprecated RN 0.47
   public List<Class<? extends JavaScriptModule>> createJSModules() {
     return Collections.emptyList();
   }
