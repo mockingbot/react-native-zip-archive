702458662cdbd1ff15f5de296a86e27e4221090f
dr-js <dr@dr.city>
2017-09-07 15:09:45 +0800

    cleanup .*ignore

--- android/src/main/java/com/rnziparchive/RNZipArchivePackage.java
+++ android/src/main/java/com/rnziparchive/RNZipArchivePackage.java
@@ -16,12 +16,10 @@ public class RNZipArchivePackage implements ReactPackage {
     modules.add(new RNZipArchiveModule(reactContext));
     return modules;
   }
-  
-  // deprecated >= RN 0.47.0
-	public List<Class<? extends JavaScriptModule>> createJSModules()
- 	{
- 		return Collections.emptyList();
- 	}  
+
+  public List<Class<? extends JavaScriptModule>> createJSModules() {
+    return Collections.emptyList();
+  }
 
   @Override
   public List<ViewManager> createViewManagers(ReactApplicationContext reactContext) {
