88250c6483e0378aaae002eec4a49718f5087f15
plrthink <plrthink@gmail.com>
2020-01-30 01:43:56 +0800

    #170 try to fix zip slip

--- android/src/main/java/com/rnziparchive/RNZipArchiveModule.java
+++ android/src/main/java/com/rnziparchive/RNZipArchiveModule.java
@@ -153,6 +153,7 @@ public class RNZipArchiveModule extends ReactContextBaseJavaModule {
           Log.d(TAG, "Zip has " + zipFile.size() + " entries");
           while (entries.hasMoreElements()) {
             final ZipEntry entry = entries.nextElement();
+            ensureZipPathSafety(new File(new File(zipFilePath).getParent() + "/" + entry.getName()), destDirectory);
             if (entry.isDirectory()) continue;
 
             StreamUtil.ProgressCallback cb = new StreamUtil.ProgressCallback() {
